31ec8c5793d2b3fcafa830017651000ae48c4b79
teja297 <143919845+teja297@users.noreply.github.com>
2024-02-07 00:19:22 +0530

    Update main.tf

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -1,8 +1,155 @@
 provider "aws" {
-    region = "us-east-1"  # Set your desired AWS region
+  region = "us-east-1"
 }
 
-resource "aws_instance" "example" {
-    ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
-    instance_type = "t2.micro"
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "MyVPC"
+  }
+}
+
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = aws_vpc.my_vpc.id
+  tags = {
+    Name = "MyIGW"
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+  tags = {
+    Name = "PublicSubnet"
+  }
+}
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+  tags = {
+    Name = "PrivateSubnet"
+  }
+}
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my_igw.id
+  }
+  tags = {
+    Name = "PublicRouteTable"
+  }
+}
+
+resource "aws_route_table_association" "public_subnet_association" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+
+resource "aws_eip" "nat_eip" {
+  domain = "vpc"
+}
+
+
+
+resource "aws_nat_gateway" "my_nat_gateway" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public_subnet.id
+  tags = {
+    Name = "MyNatGateway"
+  }
+}
+
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.my_nat_gateway.id
+  }
+  tags = {
+    Name = "PrivateRouteTable"
+  }
+}
+
+
+resource "aws_route_table_association" "private_subnet_association" {
+  subnet_id      = aws_subnet.private_subnet.id
+  route_table_id = aws_route_table.private_route_table.id
+}
+
+
+resource "aws_instance" "private_instance" {
+  ami             = "ami-0277155c3f0ab2930"
+  instance_type   = "t2.micro"
+  key_name        = "new_key"
+  subnet_id       = aws_subnet.private_subnet.id
+  security_groups = [aws_security_group.allow_ssh.id]
+  tags = {
+    Name = "PrivateInstance"
+  }
+}
+resource "aws_instance" "bastion_host" {
+  ami                         = "ami-0277155c3f0ab2930"
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.public_subnet.id
+  key_name                    = "new_key"
+  associate_public_ip_address = true
+  tags = {
+    Name = "BastionHost"
+  }
+  security_groups = [aws_security_group.allow_ssh.id]
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow-ssh"
+  description = "Allow inbound SSH traffic"
+  vpc_id      = aws_vpc.my_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allowing SSH traffic from anywhere
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "null_resource" "example_provisioner" {
+  # This is the trigger to apply the provisioners when the resource changes
+  triggers = {
+    # Use a unique value here to force the provisioners to run every time
+    instance_id = aws_instance.private_instance.id
+  }
+  provisioner "remote-exec" {
+    # Connect to the existing instance using the IP or hostname
+    connection {
+      type        = "ssh"
+      user        = "ec2-user"
+      host        = aws_instance.bastion_host.public_ip
+      private_key = file("${path.module}/private_keys/new_key.pem")
+    }
+
+    inline = [
+      "sudo yum update –y",
+      "sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo",
+      "sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key",
+      "sudo yum upgrade",
+      "sudo dnf install java-17-amazon-corretto -y",
+      "sudo yum install jenkins -y",
+      "sudo service jenkins enable",
+      "sudo service jenkins start",
+      "sudo service jenkins status",
+      "sudo cat /var/lib/jenkins/secrets/initialAdminPassword",
+    ]
+  }
 }
